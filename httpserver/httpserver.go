44a87e9eeb201a9d37dc27485de071420399b285
Xon Vanetta <xon.vanetta@gmail.com>
2018-08-28 22:10:30 +0200

    added logging config to the httpserver

--- httpserver/httpserver.go
+++ httpserver/httpserver.go
@@ -4,6 +4,11 @@ import (
 	"math"
 	"net/http"
 	"sync/atomic"
+	"time"
+
+	"github.com/jonaz/ginlogrus"
+
+	"github.com/sirupsen/logrus"
 
 	"github.com/gin-gonic/gin"
 	"github.com/urfave/cli"
@@ -16,7 +21,16 @@ type CLIFunc func(*cli.Context, *gin.Context)
 
 //New applies the callback to the gin instance and returns the error from gin
 func New(c *cli.Context, callback CLIFunc) error {
-	g := gin.Default()
+	g := gin.New()
+
+	g.Use(gin.Recovery())
+
+	logger, err := getLogger(c)
+	if err != nil {
+		return err
+	}
+
+	g.Use(ginlogrus.New(logger, time.RFC3339))
 
 	g.GET(c.String("url"), unstable(c, callback))
 	g.POST(c.String("url"), unstable(c, callback))
@@ -39,6 +53,23 @@ func unstable(c *cli.Context, callback CLIFunc) gin.HandlerFunc {
 	}
 }
 
+func getLogger(c *cli.Context) (*logrus.Logger, error) {
+	logger := logrus.StandardLogger()
+
+	if c.Bool("json-logging") {
+		logger.Formatter = &logrus.JSONFormatter{}
+	}
+	loglevel, err := logrus.ParseLevel(c.String("log-level"))
+
+	if err != nil {
+		return nil, err
+	}
+
+	logger.SetLevel(loglevel)
+
+	return logger, nil
+}
+
 //CLIFlags the flags for the default webserver
 func CLIFlags(urlPath string) []cli.Flag {
 	return []cli.Flag{
@@ -56,6 +87,15 @@ func CLIFlags(urlPath string) []cli.Flag {
 			Name:  "interval, i",
 			Value: 1,
 			Usage: "How often the request should return an ok message",
+		},
+		cli.StringFlag{
+			Name:  "log-level",
+			Value: "info",
+			Usage: "Log level to set on the httpserver (info, warn, error)",
+		},
+		cli.BoolFlag{
+			Name:  "json-logging",
+			Usage: "If you wants the logs to be in json",
 		},
 	}
 }
